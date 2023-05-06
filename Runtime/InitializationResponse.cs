017b6a9b02fe908968a1360cca284098d83f7867
Denis Kotenko <d.kotenko@rj-games.com>
2023-05-06 14:30:18 +0400

    in apps

--- Runtime/InitializationResponse.cs
+++ Runtime/InitializationResponse.cs
@@ -5,6 +5,5 @@ namespace RatYandex.Runtime
     public class InitializationResponse
     {
         [JsonProperty("is_player_authorized")] public bool IsPlayerAuthorized { get; set; } 
-        [JsonProperty("is_payments_initialized")] public bool IsPaymentsInitialized { get; set; } 
     }
 }
--- Runtime/InitializePaymentsResult.cs
+++ Runtime/InitializePaymentsResult.cs
@@ -4,6 +4,23 @@ namespace RatYandex.Runtime
 {
     public class InitializePaymentsResult 
     {
-        [JsonProperty("purchases")] public string Purchases { get; set; }
+        [JsonProperty("purchases")] public object[] Purchases { get; set; }
+        [JsonProperty("catalog")] public YaInAppCatalogItem[] Catalog { get; set; }
+    }
+    
+    public class YaInAppCatalogItem
+    {
+        [JsonProperty("id")] public string ID { get; set; }
+        [JsonProperty("title")] public string Title { get; set; }
+        [JsonProperty("description")] public string Description { get; set; }
+        [JsonProperty("image_uri")] public string ImageUri { get; set; }
+        [JsonProperty("price")] public string Price { get; set; }
+        [JsonProperty("price_value")] public string PriceValue { get; set; }
+        [JsonProperty("price_currency_code")] public string PriceCurrencyCode { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(ID)}: {ID}, {nameof(Title)}: {Title}, {nameof(Description)}: {Description}, {nameof(ImageUri)}: {ImageUri}, {nameof(Price)}: {Price}, {nameof(PriceValue)}: {PriceValue}, {nameof(PriceCurrencyCode)}: {PriceCurrencyCode}";
+        }
     }
 }
